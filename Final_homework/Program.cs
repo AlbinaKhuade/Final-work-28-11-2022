be7b3df573c1df399e5ba18265079ccab172accb
Albina <albine89@mail.ru>
2022-11-29 22:38:48 +0300

    Final_homework

---
+++ Final_homework/Program.cs
@@ -0,0 +1,41 @@
+﻿string[] array = new string[] { "test", "123", "0" };
+
+void PrintArray(string[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) Console.Write($"{arr[i]}, ");
+        else Console.Write($"{arr[i]}");
+    }
+    Console.Write("]");
+}
+
+string[] CreateNewArray(string[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i].Length <= 3)
+        {
+            count++;
+        }
+    }
+
+    string[] newArr = new string[count];
+    int j = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i].Length <= 3)
+        {
+            newArr[j] = arr[i];
+            j++;
+        }
+    }
+    return newArr;
+}
+
+string[] newArray = CreateNewArray(array);
+PrintArray(array);
+Console.Write(" -> ");
+PrintArray(newArray);
